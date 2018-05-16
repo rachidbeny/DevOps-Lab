809b33bf6792f807a7d1dcd3c39ddd9520414c6e
PT-Jaloit <39120700+PT-Jaloit@users.noreply.github.com>
2018-05-16 22:04:34 +0300

    Development (#6)
    * Init
    
    * Added kuberteness
    
    * Harjoitusten valmistelu
    
    * Added merge branch with conflicts
    
    * test
    
    * test
    
    * Jenkins
    
    * Lisätty seuraavat osiot
    
    * Lisätty ohjeita
    
    * Poistettu vanhat osiot
    
    * Fixed line breaks
    
    * Line breaks
    
    * Siirretty ohjeet alikansioihin
    
    * Päivitetty ohjeet
    
    * Lisätty blue ocean osa
    
    * Lisätty Jenkinsin linux asennuksen ohje
    
    * Päivitetty Harjoitusta 1
    
    * Korjattu harjoitus 3
    
    * IaC harjoitus
    
    * korjattu koodiblockit
    
    * korjattu harjoitus 4

--- Harjoitus 3/Docker/bootstrap.sh
+++ Harjoitus 3/Docker/bootstrap.sh
@@ -1,5 +1,3 @@
-# Install Docker-CE
-
 sudo apt-get update
 sudo apt-get -y install \
     apt-transport-https \
@@ -7,17 +5,17 @@ sudo apt-get -y install \
     curl \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce
-sudo docker run hello-world
 
-# Install Docker Compose
-
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo docker-compose --version
+sudo apt-get -y install git
+sudo mkdir /var/tmp/demo
+cd /var/tmp/demo
+sudo git clone https://github.com/docker-training/webapp.git
+cd webapp
+sudo docker build -t testwebapp .
+sudo docker run -d -p 80:5000 testwebapp
--- Harjoitus 4/README.md
+++ Harjoitus 4/README.md
@@ -1,2 +1,25 @@
 # Harjoitus 4 - Infrastructure as Code
+## AWS EC2 - Palvelimen luonti Ansiblella
+Asenna vaadittavat ohjelmistot
+```
+sudo apt-get update
+sudo apt-get -y install ansible
+sudo apt-get -y install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install boto
+sudo apt-get install ansible
+```
 
+Määritä AWS Api avaimet (https://console.aws.amazon.com/iam/home?#/security_credential):
+```
+export AWS_ACCESS_KEY_ID="XXXXXXXXXXXXXXXXXXXXXXX"
+export AWS_SECRET_ACCESS_KEY="XXXXXXXXXXXXXXXXXXXXXXX"
+```
+
+Kopioi ec2.yaml palvelimelle
+
+Luo uusi palvelin
+
+```ansible-playbook -i ./ansible-hosts ec2.yml```
+
+Source: https://www.agix.com.au/build-an-ec2-using-ansible-step-by-step/
