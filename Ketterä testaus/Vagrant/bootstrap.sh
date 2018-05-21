4cfba4aeb0c175665b769ba5dd3b6c3ca68488c7
PT-Jaloit <39120700+PT-Jaloit@users.noreply.github.com>
2018-05-21 16:21:20 +0300

    Testaus

--- Ketterä testaus/Vagrant/bootstrap.sh
+++ Ketterä testaus/Vagrant/bootstrap.sh
@@ -10,6 +10,10 @@ sudo apt-get -y install git
 sudo mkdir /var/tmp/demo
 cd /var/tmp/demo
 sudo git clone https://bitbucket.org/robotframework/webdemo.git
-sudo wget https://raw.githubusercontent.com/PT-Jaloit/DevOps-Lab/master/Ketterä%20testaus/Docker/Dockerfile
+
+sudo git clone https://github.com/ppodgorsek/docker-robot-framework.git
+#sudo wget https://raw.githubusercontent.com/PT-Jaloit/DevOps-Lab/master/Ketterä%20testaus/Docker/Dockerfile
+cd docker-robot-framework
 sudo docker build -t robot .
-sudo docker run -p 80:7272 robot
+cd ../webdemo
+sudo docker run -v ${PWD}:/opt/robotframework/reports:Z -v ${PWD}/login_tests:/opt/robotframework/tests:Z -e BROWSER=firefox -p 80:7272 robot
