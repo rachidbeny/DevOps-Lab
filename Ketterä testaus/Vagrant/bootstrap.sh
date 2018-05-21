b8ea87e2851a56c49a3a28bd956ca71d3e185ce0
PT-Jaloit <39120700+PT-Jaloit@users.noreply.github.com>
2018-05-21 11:58:00 +0300

    Testaus

---
+++ Ketterä testaus/Vagrant/bootstrap.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+
+sudo apt-get -y install git
+sudo mkdir /var/tmp/demo
+cd /var/tmp/demo
+sudo git clone https://bitbucket.org/robotframework/webdemo.git
+cd WebDemo
+wget https://raw.githubusercontent.com/PT-Jaloit/DevOps-Lab/master/Ketterä%20testaus/Docker/Dockerfile.txt
+sudo docker build -t robot .
+sudo docker run -d -p 80:7272 robot
