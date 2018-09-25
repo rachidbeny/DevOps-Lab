e2ee54346b93214a72a4b82cbc05c61dfade6b55
PT-Jaloit <39120700+PT-Jaloit@users.noreply.github.com>
2018-09-25 21:04:57 +0300

    Updated docker file

--- Harjoitus 2/Vagrant/bootstrap.sh
+++ Harjoitus 2/Vagrant/bootstrap.sh
@@ -6,5 +6,13 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get -y install docker-ce
 
-sudo docker pull jenkins/jenkins:lts
-sudo docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+sudo docker pull jenkinsci/blueocean
+sudo docker run \
+  --rm \
+  -u root \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -v jenkins-data:/var/jenkins_home \ 
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$HOME":/home \ 
+  jenkinsci/blueocean
