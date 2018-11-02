8052b8a3b57589d98c5e255cf02dafa9b9cb2b69
PT-Jaloit <39120700+PT-Jaloit@users.noreply.github.com>
2018-11-02 14:26:47 +0200

    Added nodeapp bootstrap

---
+++ Harjoitus 3/Docker/bootstrap.1.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+
+sudo apt-get -y install git
+sudo mkdir /var/tmp/demo
+cd /var/tmp/demo
+sudo git clone https://github.com/PT-Jaloit/Test2.git
+cd Test2/NodeWebApp
+sudo docker build -t nodewebapp .
+sudo docker run -d -p 80:3000 nodewebapp
